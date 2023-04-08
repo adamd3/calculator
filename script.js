aa6d0e70c14916a785b8e0f39f2e4583bf42e0a2
Adam Dinan <adam1989ie@gmail.com>
2023-04-08 21:20:52 +0100

    Add basic query selectors and event listeners

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let operator = null;
+let firstNum = null;
+let secondNum = null;
+let displayVal = null;
+
+const buttons = document.querySelectorAll('button');
+const operators = document.querySelectorAll('button--operator');
+const display = document.getElementById('display');
+
 function add(a, b) {
   return a + b;
 }
@@ -14,11 +23,44 @@ function divide(a, b) {
   return a / b;
 }
 
-let operator = null;
-let firstNum = null;
-let secondNum = null;
 
 function operate(operator, firstNum, secondNum) {
   return operator(firstNum, secondNum);
 }
+
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (button.id == 'clear') {
+      display.textContent = '';
+    } else if (button.id == 'equals') {
+      display.textContent = operate(operator, firstNum, secondNum);
+    } else {
+      display.textContent += button.textContent;
+    }
+  });
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener('click', () => {
+    firstNum = display.textContent;
+    display.textContent = '';
+    switch (operator.id) {
+      case 'add':
+        operator = add;
+        break;
+      case 'subtract':
+        operator = subtract;
+        break;
+      case 'multiply':
+        operator = multiply;
+        break;
+      case 'divide':
+        operator = divide;
+        break;
+    }
+  });
+});
+
+// display.textContent = operate(operator, firstNum, secondNum);
 
