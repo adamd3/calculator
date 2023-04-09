52044ed4022a7bf5ec7376b4504cbdc8606cc4d5
Adam Dinan <adam1989ie@gmail.com>
2023-04-09 12:34:33 +0100

    Remove event listener

--- script.js
+++ script.js
@@ -107,24 +107,3 @@ buttons.forEach((button) => {
     }
   });
 });
-
-// operators.forEach((operator) => {
-//   operator.addEventListener('click', () => {
-//     firstNum = display.textContent;
-//     display.textContent = '';
-//     switch (operator.id) {
-//       case 'add':
-//         operator = add;
-//         break;
-//       case 'subtract':
-//         operator = subtract;
-//         break;
-//       case 'multiply':
-//         operator = multiply;
-//         break;
-//       case 'divide':
-//         operator = divide;
-//         break;
-//     }
-//   });
-// });
